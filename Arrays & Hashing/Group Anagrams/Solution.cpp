449f9076f33526f9f099784269df9ba4f142a3af
Sage <Raymondzheng2000@gmail.com>
2026-08-21 23:54:32 -0400

    Group Anagrams

---
+++ Arrays & Hashing/Group Anagrams/Solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+        vector<vector<string>> groupAnagrams(vector<string>& strs) {
+            unordered_map<string, vector<string>> groups;
+    
+            for (string s : strs) {
+                vector<int> count(26, 0);
+    
+                for (char c : s) {
+                    count[c - 'a']++;
+                }
+    
+                string key;
+    
+                for (int x : count) {
+                    key += '#' + to_string(x);
+                }
+    
+                groups[key].emplace_back(s);
+            }
+    
+            vector<vector<string>> result;
+    
+            for (auto& [key, group] : groups) {
+                result.emplace_back(group);
+            }
+    
+            return result;
+        }
+    };
