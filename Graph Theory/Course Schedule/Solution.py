116ad24a70fc01e86592662ac9dbbac6b53a3bad
Raymond1134 <raymondzheng2000@gmail.com>
2026-09-06 16:36:12 -0400

    Course Schedule

---
+++ Graph Theory/Course Schedule/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        degrees = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+            degrees[a] += 1
+        
+        degreeZeroNodes = []
+
+        for i in range(numCourses):
+            if degrees[i] == 0: degreeZeroNodes.append(i)
+        
+        while degreeZeroNodes:
+            node = degreeZeroNodes.pop()
+
+            for neighbour in graph[node]:
+                degrees[neighbour] -= 1
+                if degrees[neighbour] == 0: degreeZeroNodes.append(neighbour)
+        
+        return all(degrees[i] == 0 for i in range(numCourses))
